a165679091c0e4aef3ae2083d9fdbf02107238ce
daiyong <daiyong@coohua.com>
2018-07-02 13:55:01 +0800

    first

---
+++ app-tpl-web/src/main/java/com/app/tpl/web/AppTplWebApplication.java
@@ -0,0 +1,12 @@
+package com.app.tpl.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTplWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTplWebApplication.class, args);
+	}
+}
